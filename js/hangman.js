efcfe448aba9f16558a878ab4687c8e69454dbd0
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-18 18:01:24 +0200

    Update hangman.js

--- js/hangman.js
+++ js/hangman.js
@@ -8,3 +8,9 @@ var words = [
 
 // Choose a random word
 var word = words[Math.floor(Math.random() * words.length)];
+
+// We set the array of answers
+var answerArray = [];
+for (var i = 0; i < word.length; i++){
+  answerArray[i] = "_";
+}
